30c228f4a4b669cbec1168307628b67ee395320e
Cami Fresch <cami.fresch@hotmail.com>
2022-12-29 22:11:16 -0300

    carrito commit

--- js/main.js
+++ js/main.js
@@ -3,13 +3,10 @@ let nombreForm = document.querySelector("#nombreyapellido");
 let dateForm = document.querySelector("#date");
 
 //Eventos
-nombreForm.addEventListener ("input", function () {
+nombreForm.addEventListener("input", function () {
   console.log(nombreForm.value);
   if (nombreForm.value === "") {
     alert("Ingrese un nombre y apellido válido");
-  }
-  else {
-    alert("correcto!")
   }
 });
 
@@ -17,9 +14,6 @@ dateForm.addEventListener("input", function () {
   console.log(dateForm.value);
   if (dateForm.value === "") {
     alert("Ingrese una fecha válida");
-  }
-  else {
-    alert("correcto!")
   }
 });
 
@@ -33,3 +27,53 @@ const mostrarInfo = formulario.addEventListener("submit", function (e) {
   <div class="alert alert-warning" role="alert">
 <h5> Muchas gracias ${nombreForm.value} te esperamos el ${dateForm.value} para realizar el servicio a eleccion.</h5></div>`;
 });
+
+
+
+// carrito
+
+const cards = document.querySelectorAll(".card");
+cards.forEach((card) => {
+  card.addEventListener("click", (e) => {
+    leerDatosServicio(e.target.parentElement);
+  });
+});
+
+
+let articulosCarrito = [];
+
+function leerDatosServicio(servicios) {
+  const infoServicio = {
+    titulo: servicios.querySelector(".card-title").textContent,
+    texto: servicios.querySelector(".card-text").textContent,
+  };
+
+  //Agregar al carrito
+  articulosCarrito = [...articulosCarrito, infoServicio];
+  console.log(articulosCarrito);
+
+  carritoHTML();
+}
+
+//Mostrar los servicios en el carrito
+const carrito = document.querySelector("#carrito");
+
+function carritoHTML() {
+  limpiarHTML();
+
+  articulosCarrito.forEach((servicios) => {
+    const row = document.createElement("p");
+    row.innerHTML = `
+    <div class="container">
+    <h5>${servicios.titulo}</h5>
+    <p>${servicios.texto}</p>
+    <button class="btn btn-danger">Eliminar</button>
+    </div>
+    `;
+    carrito.appendChild(row);
+  });
+}
+
+function limpiarHTML() {
+  carrito.innerHTML = "";
+}
